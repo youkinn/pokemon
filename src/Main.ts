8bcc2d125544e346c3f13842e8d1556dabf6f5a6
叶鑫 <03338277@163.com>
2018-03-15 21:34:39 +0800

    init

--- src/Main.ts
+++ src/Main.ts
@@ -14,7 +14,7 @@
 //       names of its contributors may be used to endorse or promote products
 //       derived from this software without specific prior written permission.
 //
-//  THIS SOFTWARE IS PROVIDED BY EGRET AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+//  THIS SOFTWARE IS PROVIDED BY EGRET AND CONTRIBUTORS 'AS IS' AND ANY EXPRESS
 //  OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
 //  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
 //  IN NO EVENT SHALL EGRET AND CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
@@ -27,27 +27,21 @@
 //
 //////////////////////////////////////////////////////////////////////////////////////
 
-class Main extends egret.DisplayObjectContainer {
-
+class Main extends eui.UILayer {
     /**
      * 加载进度界面
-     * Process interface loading
+     * loading process interface
      */
     private loadingView: LoadingUI;
 
-    public constructor() {
-        super();
-        this.addEventListener(egret.Event.ADDED_TO_STAGE, this.onAddToStage, this);
-    }
+    private currentPage: string;
+    private createRoleUI: CreateRoleUI;
 
-    private onAddToStage(event: egret.Event) {
+    protected createChildren(): void {
+        super.createChildren();
 
         egret.lifecycle.addLifecycleListener((context) => {
             // custom lifecycle plugin
-
-            context.onUpdate = () => {
-                // console.log('hello,world')
-            }
         })
 
         egret.lifecycle.onPause = () => {
@@ -58,224 +52,118 @@ class Main extends egret.DisplayObjectContainer {
             egret.ticker.resume();
         }
 
-
+        //inject the custom material parser
+        //注入自定义的素材解析器
+        let assetAdapter = new AssetAdapter();
+        egret.registerImplementation('eui.IAssetAdapter', assetAdapter);
+        egret.registerImplementation('eui.IThemeAdapter', new ThemeAdapter());
+        //Config loading process interface
         //设置加载进度界面
-        //Config to load process interface
         this.loadingView = new LoadingUI();
         this.stage.addChild(this.loadingView);
-
+        // initialize the Resource loading library
         //初始化Resource资源加载库
-        //initiate Resource loading library
         RES.addEventListener(RES.ResourceEvent.CONFIG_COMPLETE, this.onConfigComplete, this);
-        RES.loadConfig("resource/default.res.json", "resource/");
+        RES.loadConfig('resource/default.res.json', 'resource/');
     }
-
     /**
-     * 配置文件加载完成,开始预加载preload资源组。
-     * configuration file loading is completed, start to pre-load the preload resource group
+     * 配置文件加载完成,开始预加载皮肤主题资源和preload资源组。
+     * Loading of configuration file is complete, start to pre-load the theme configuration file and the preload resource group
      */
     private onConfigComplete(event: RES.ResourceEvent): void {
         RES.removeEventListener(RES.ResourceEvent.CONFIG_COMPLETE, this.onConfigComplete, this);
+        // load skin theme configuration file, you can manually modify the file. And replace the default skin.
+        //加载皮肤主题配置文件,可以手动修改这个文件。替换默认皮肤。
+        let theme = new eui.Theme('resource/default.thm.json', this.stage);
+        theme.addEventListener(eui.UIEvent.COMPLETE, this.onThemeLoadComplete, this);
+
         RES.addEventListener(RES.ResourceEvent.GROUP_COMPLETE, this.onResourceLoadComplete, this);
         RES.addEventListener(RES.ResourceEvent.GROUP_LOAD_ERROR, this.onResourceLoadError, this);
         RES.addEventListener(RES.ResourceEvent.GROUP_PROGRESS, this.onResourceProgress, this);
         RES.addEventListener(RES.ResourceEvent.ITEM_LOAD_ERROR, this.onItemLoadError, this);
-        RES.loadGroup("create_role");
+        RES.loadGroup('preload');
     }
-
+    private isThemeLoadEnd: boolean = false;
+    /**
+     * 主题文件加载完成,开始预加载
+     * Loading of theme configuration file is complete, start to pre-load the 
+     */
+    private onThemeLoadComplete(): void {
+        this.isThemeLoadEnd = true;
+        this.createScene();
+    }
+    private isResourceLoadEnd: boolean = false;
     /**
      * preload资源组加载完成
-     * Preload resource group is loaded
+     * preload resource group is loaded
      */
-    private onResourceLoadComplete(event: RES.ResourceEvent) {
-        if (event.groupName == "create_role") {
+    private onResourceLoadComplete(event: RES.ResourceEvent): void {
+        if (event.groupName == 'preload') {
             this.stage.removeChild(this.loadingView);
             RES.removeEventListener(RES.ResourceEvent.GROUP_COMPLETE, this.onResourceLoadComplete, this);
             RES.removeEventListener(RES.ResourceEvent.GROUP_LOAD_ERROR, this.onResourceLoadError, this);
             RES.removeEventListener(RES.ResourceEvent.GROUP_PROGRESS, this.onResourceProgress, this);
             RES.removeEventListener(RES.ResourceEvent.ITEM_LOAD_ERROR, this.onItemLoadError, this);
-            this.createGameScene();
+            this.isResourceLoadEnd = true;
+            this.createScene();
         }
     }
-
-    /**
-     * 资源组加载出错
-     *  The resource group loading failed
-     */
-    private onItemLoadError(event: RES.ResourceEvent) {
-        console.warn("Url:" + event.resItem.url + " has failed to load");
+    private createScene() {
+        if (this.isThemeLoadEnd && this.isResourceLoadEnd) {
+            this.startCreateScene();
+        }
     }
-
     /**
      * 资源组加载出错
      *  The resource group loading failed
      */
-    private onResourceLoadError(event: RES.ResourceEvent) {
+    private onItemLoadError(event: RES.ResourceEvent): void {
+        console.warn('Url:' + event.resItem.url + ' has failed to load');
+    }
+    /**
+     * 资源组加载出错
+     * Resource group loading failed
+     */
+    private onResourceLoadError(event: RES.ResourceEvent): void {
         //TODO
-        console.warn("Group:" + event.groupName + " has failed to load");
+        console.warn('Group:' + event.groupName + ' has failed to load');
         //忽略加载失败的项目
-        //Ignore the loading failed projects
+        //ignore loading failed projects
         this.onResourceLoadComplete(event);
     }
-
     /**
      * preload资源组加载进度
-     * Loading process of preload resource group
+     * loading process of preload resource
      */
-    private onResourceProgress(event: RES.ResourceEvent) {
-        if (event.groupName == "preload") {
+    private onResourceProgress(event: RES.ResourceEvent): void {
+        if (event.groupName == 'preload') {
             this.loadingView.setProgress(event.itemsLoaded, event.itemsTotal);
         }
     }
-
     private textfield: egret.TextField;
-
     /**
-     * 创建游戏场景
-     * Create a game scene
+     * 创建场景界面
+     * Create scene interface
      */
-    private createGameScene() {
-
-        let bg = this.createBitmapByName('create_role_bg_jpg');
-        this.addChild(bg);
-        let stageW = this.stage.stageWidth;
-        let stageH = this.stage.stageHeight;
-        bg.width = stageW;
-        bg.height = stageH;
-
-        let create_chose_role = new egret.Bitmap(RES.getRes('create_chose_role_png'));
-        create_chose_role.x = 160;
-        create_chose_role.y = 0;
-        create_chose_role.width = 320;
-        this.addChild(create_chose_role);
-
-        let back_server = new egret.Bitmap(RES.getRes('back_server_png'));
-        back_server.x = 500;
-        back_server.y = 5;
-        this.addChild(back_server);
-
-        const roleWidth = 158;
-        const roleHeight = roleWidth / (118 / 227);
-        const roleHeight2 = roleWidth / (118 / 179);
-        const roles = [
-            {
-                pic: 'role_1_png',
-                pic2: '21000_12_1_png',
-                x: 0,
-                y: 50,
-                width: roleWidth,
-                height: roleHeight
-            }, {
-                pic: 'role_2_png',
-                pic2: '21000_12_2_png',
-                x: 160,
-                y: 95,
-                width: roleWidth,
-                height: roleHeight2
-            }, {
-                pic: 'role_3_png',
-                pic2: '21000_12_3_png',
-                x: 320,
-                y: 95,
-                width: roleWidth,
-                height: roleHeight2
-            }, {
-                pic: 'role_4_png',
-                pic2: '21000_12_4_png',
-                x: 480,
-                y: 50,
-                width: roleWidth,
-                height: roleHeight
-            }
-        ];
-        const that = this;
-        let role;
-        let rolePic2;
-        let item;
-        let arr = [];
-        const msec = 1050;
-        for (let i = 0, j = roles.length; i < j; i++) {
-            item = roles[i];
-            role = new egret.Bitmap(RES.getRes(item.pic));
-            role.x = item.x;
-            role.y = item.y;
-            role.width = item.width;
-            role.height = item.height;
-            rolePic2 = new egret.Bitmap(RES.getRes(item.pic2));
-            arr.push(rolePic2);
-
-            this.addChild(role);
-            role.touchEnabled = true;
-            (function (i, role, rolePic2) {
-                role.addEventListener(egret.TouchEvent.TOUCH_TAP, function () {
-                    for (let i = 0, j = arr.length; i < j; i++) {
-                        arr[i].parent && arr[i].parent.removeChild(arr[i]);
-                    }
-                    jump(rolePic2);
-                }, that);
-            })(i, role, rolePic2);
-            if (i == 0) {
-                jump(rolePic2);
-            }
-        }
-
-        let roleNameContainer = new egret.Sprite();
-        roleNameContainer.x = 160;
-        roleNameContainer.y = 800;
-        roleNameContainer.width = 320;
-        roleNameContainer.height = 50;
-        this.addChild(roleNameContainer);
-
-        let role_name_bg_png = new egret.Bitmap(RES.getRes('role_name_bg_png'));
-        role_name_bg_png.x = 160;
-        role_name_bg_png.y = 800;
-        role_name_bg_png.width = 320;
-        this.addChild(role_name_bg_png);
-
-        let label: egret.TextField = new egret.TextField();
-        label.x = 180;
-        label.y = 810;
-        label.text = '嘎啦嘎啦';
-        label.textColor = 0xffffff;
-        label.size = 24;
-        label.textAlign = egret.HorizontalAlign.CENTER;
-        label.verticalAlign = egret.VerticalAlign.MIDDLE;
-        label.touchEnabled = true;
-        roleNameContainer.addChild(label); 
-
-        label.addEventListener(egret.TouchEvent.TOUCH_BEGIN, (e) => {
-            label.setFocus();
-        }, this);
-
-        function jump(obj) {
-            obj.width = 240;
-            obj.height = obj.width / (212 / 349);
-            obj.x = (640 - obj.width) / 2;
-            obj.y = 370;
-            that.addChild(obj);
-
-            egret.Tween.get(obj, { loop: true })
-                .to({ y: obj.y + 10 }, msec / 2, egret.Ease.backInOut)
-                .to({ y: obj.y }, msec / 2, egret.Ease.backInOut);
-        }
+    protected startCreateScene(): void {
+        this.createRoleUI = new CreateRoleUI();
+        this.addChild(this.createRoleUI);
     }
-
     /**
      * 根据name关键字创建一个Bitmap对象。name属性请参考resources/resource.json配置文件的内容。
      * Create a Bitmap object according to name keyword.As for the property of name please refer to the configuration file of resources/resource.json.
      */
-    private createBitmapByName(name: string) {
+    private createBitmapByName(name: string): egret.Bitmap {
         let result = new egret.Bitmap();
         let texture: egret.Texture = RES.getRes(name);
         result.texture = texture;
         return result;
     }
-
     /**
      * 描述文件加载成功，开始播放动画
      * Description file loading is successful, start to play the animation
      */
-    private startAnimation(result: string[]) {
+    private startAnimation(result: Array<any>): void {
         let parser = new egret.HtmlTextParser();
 
         let textflowArr = result.map(text => parser.parse(text));
@@ -292,14 +180,24 @@ class Main extends egret.DisplayObjectContainer {
             // Switch to described content
             textfield.textFlow = textFlow;
             let tw = egret.Tween.get(textfield);
-            tw.to({ "alpha": 1 }, 200);
+            tw.to({ 'alpha': 1 }, 200);
             tw.wait(2000);
-            tw.to({ "alpha": 0 }, 200);
+            tw.to({ 'alpha': 0 }, 200);
             tw.call(change, this);
         };
 
         change();
     }
+
+    /**
+     * 点击按钮
+     * Click the button
+     */
+    private onButtonClick(e: egret.TouchEvent) {
+        let panel = new eui.Panel();
+        panel.title = 'Title';
+        panel.horizontalCenter = 0;
+        panel.verticalCenter = 0;
+        this.addChild(panel);
+    }
 }
-
-
---
+++ src/pages/createRole.page.ts
@@ -0,0 +1,75 @@
+class CreateRoleUI extends eui.Component {
+    private rolePic: eui.Image;
+    private rdRole1: eui.RadioButton;
+    private rdRole2: eui.RadioButton;
+    private rdRole3: eui.RadioButton;
+    private rdRole4: eui.RadioButton;
+    private btnRoles: eui.RadioButton[];
+    private btnCrap: eui.RadioButton;
+    private txtName: eui.TextInput;
+    private nameConfig;
+    private sex = {
+        MALE: 'male',
+        FEMALE: 'female',
+    };
+
+    constructor() {
+        super();
+        this.addEventListener(eui.UIEvent.COMPLETE, this.uiCompHandler, this);
+        this.skinName = 'resource/skins/createRoleUISkin.exml';
+    }
+
+    private uiCompHandler(e: egret.Event): void {
+        this.loadNameConfig();
+
+
+        let item = {};
+        this.btnRoles = [this.rdRole1, this.rdRole2, this.rdRole3, this.rdRole4];
+        const rolePics = ['21000_12_1_png', '21000_12_2_png', '21000_12_3_png', '21000_12_4_png'];
+        for (let i = 0, j = this.btnRoles.length; i < j; i++) {
+            this.btnRoles[i].addEventListener(egret.TouchEvent.TOUCH_TAP, function () {
+                this.touchHandler(rolePics[i]);
+            }, this);
+        }
+        this.btnRoles[0].dispatchEventWith(egret.TouchEvent.TOUCH_TAP, false, rolePics[0]);
+
+        this.btnCrap.addEventListener(egret.TouchEvent.TOUCH_TAP, function (e: egret.TouchEvent) {
+            const name = this.createRoleName(this.sex.MALE);
+            this.txtName.text = name;
+            console.log(name);
+        }, this);
+    }
+
+    private touchHandler(source): void {
+        this.rolePic.source = source;
+        this.addChild(this.rolePic);
+        egret.Tween.get(this.rolePic, { loop: true })
+            .to({ y: this.rolePic.y + 10 }, 400, egret.Ease.backInOut)
+            .to({ y: this.rolePic.y }, 400, egret.Ease.backInOut);
+    }
+
+    private loadNameConfig(): void {
+        var urlloader: egret.URLLoader = new egret.URLLoader();
+        var urlreq: egret.URLRequest = new egret.URLRequest();
+        urlreq.url = "resource/config/nameConfig.json";
+        urlloader.load(urlreq);
+        urlloader.addEventListener(egret.Event.COMPLETE, this.onComplete, this);
+    }
+
+    // 读取配置文件完成
+    private onComplete(event: egret.Event): void {
+        var data = event.target.data;
+        this.nameConfig = JSON.parse(data);
+    }
+
+    /**
+     * 随机产生角色名字
+     */
+    private createRoleName(sex: string): string {
+        const nameList = this.nameConfig[sex];
+        const index = Math.random();
+        const firstName = nameList['first_name'][parseInt(index * nameList['first_name'].length + '')];
+        const secondName = nameList['second_name'][parseInt(index * nameList['second_name'].length + '')];
+        return firstName + secondName;
+    }
+}
