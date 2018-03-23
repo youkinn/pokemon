a7c7b7f46e1821227f158558e367f40947043d87
叶鑫 <03338277@163.com>
2018-03-23 18:05:00 +0800

    itemrenderer 内嵌

--- src/pages/ChanceUI.ts
+++ src/pages/ChanceUI.ts
@@ -1,44 +1,26 @@
 class ChanceUI extends eui.Component {
-    private listHeros: eui.List;
-    private scrListHeros: eui.Scroller;
+  private listActivity: eui.List;
 
-    constructor() {
-        super();
-        this.addEventListener(eui.UIEvent.COMPLETE, this.uiCompHandler, this);
-        this.skinName = 'resource/skins/chanceUISkin.exml';
+  constructor() {
+    super();
+    this.addEventListener(eui.UIEvent.COMPLETE, this.uiCompHandler, this);
+    this.skinName = 'resource/skins/chanceUISkin.exml';
+  }
+
+  private uiCompHandler(): void {
+    console.log('\t\t ChanceUI uiCompHandler');
+
+    /// 填充数据
+    let dsListHeros: Array<Object> = [];
+    for (let i = 1, j = 14; i < j; i++) {
+      dsListHeros.push({
+        image: 'activity' + i + '_png'
+      });
     }
+    this.listActivity.dataProvider = new eui.ArrayCollection(dsListHeros);
+  }
 
-    private uiCompHandler(): void {
-        console.log('\t\tHerosUI uiCompHandler');
-
-        /// 填充数据
-        let dsListHeros: Array<Object> = [];
-        for (let i = 1, j = 14; i < j; i++) {
-            dsListHeros.push({
-                image: 'activity' + i + '_png'
-            });
-        }
-        this.listHeros.dataProvider = new eui.ArrayCollection(dsListHeros);
-        this.listHeros.itemRenderer = ChanceListIRSkin;
-
-        this.scrListHeros.viewport.validateNow();
-        this.scrListHeros.viewport.scrollV = 0;
-    }
-
-    protected createChildren(): void {
-        super.createChildren();
-    }
-}
-
-class ChanceListIRSkin extends eui.ItemRenderer {
-
-    constructor() {
-        super();
-        this.skinName = 'resource/skins/chanceItemSkin.exml';
-    }
-
-    protected createChildren(): void {
-        super.createChildren();
-    }
-
+  protected createChildren(): void {
+    super.createChildren();
+  }
 }
